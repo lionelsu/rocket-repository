f7b026e3d46e3913eaf32e1dad7043fa02a1c46f
lionelsu <lionel_suicune@hotmail.com>
2022-10-04 11:47:19 -0400

    add player obj

--- Stage 05/Desafio 01 - FocusTimer/main.js
+++ Stage 05/Desafio 01 - FocusTimer/main.js
@@ -1,6 +1,8 @@
 import { musics } from './modules/sounds.js'
 
-const forest = musics('Floresta', 0)
-const rain = musics('Chuva', 1)
-const house = musics('Casa', 2)
-const fire = musics('Lareira', 3)
+const musicPlayer = {
+  forest: musics('Floresta', 0),
+  rain: musics('Chuva', 1),
+  house: musics('Casa', 2),
+  fire: musics('Lareira', 3)
+}
--- Stage 05/Desafio 01 - FocusTimer/modules/sounds.js
+++ Stage 05/Desafio 01 - FocusTimer/modules/sounds.js
@@ -2,6 +2,7 @@ import { query } from './elements.js'
 
 export function musics(musicType, number) {
   const music = new Audio(`./assets/sounds/${musicType}.wav`)
+  music.loop = true
   return {
     musicObj: query('.music').children[number].addEventListener('click', () => {
       music.play()
